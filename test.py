e0df0a94ca3724f23b4727f9266ea88f77670fcb
contodo <13289265706@163.com>
2019-07-01 22:56:07 +0800

    leetcode

---
+++ test.py
@@ -0,0 +1,29 @@
+def lengthOfLongestSubstring(s):
+    ls = []
+    l = len(s)
+    if l == 1:
+        return 1
+    if l == 2:
+        if s[0] == s[1]:
+            return 1
+        else:
+            return 2
+    for i in range(l):
+        for j in range(i+1, l):
+            if s[j] in s[i: j]:
+                print(s[j], s[i: j])
+                ls.append(s[i: j])
+                break
+            if s[j] not in s[i: j] and j == l - 1:
+                ls.append(s[i: j + 1])
+    los = 0
+    print(ls)
+    for sub in ls:
+        los = max([los, len(sub)])
+
+    return los
+
+
+s = "aab"
+print(lengthOfLongestSubstring(s))
+
